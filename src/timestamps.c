a72c6dcc33165e7e868ab16242fae9e1b98ea7db
Nate <natewardbusiness@gmail.com>
2026-08-27 12:45:36 -0400

    feat: complete simple timestamp operations

--- src/timestamps.c
+++ src/timestamps.c
@@ -4,19 +4,20 @@
 
 bool timestamp_equal(uint64_t first, uint64_t second)
 {
-    return true;
+    return first == second;
 }
 
 bool timestamp_is_before(uint64_t first, uint64_t second)
 {
-    return true;
+    return first < second;
 }
 
 bool timestamp_is_after(uint64_t first, uint64_t second)
 {
-    return true;
+    return first > second;
 }
 
+/** TODO: implement after filetime to datetime conversion is complete */
 bool timestamp_low_digits_zeroed(uint64_t timestamp)
 {
     return true;
