e63200c1ba3ba51541d7b75926402a23d88eadc8
Nate <natewardbusiness@gmail.com>
2026-08-28 14:41:19 -0400

    feat: implement timestamp rollback rule

--- src/rules.c
+++ src/rules.c
@@ -5,16 +5,24 @@
 //#include "attributes.h"
 #include "timestamps.h"
 
+#define SI_FN_MISMATCH_THRESHOLD 3
+#define TIMESTAMP_ROLLBACK_THRESHOLD 2
 #define ZEROED_TIMESTAMP_DIGITS 9
 #define ZEROED_TIMESTAMP_MINIMUM_MATCHES 2
-#define SI_FN_MISMATCH_THRESHOLD 3
 
 /**
  * @brief Detects if a variable number of timestamps in the $SI and $FN attributes don't match.
  * @param metadata Pointer to the record_metadata structure containing parsed attributes.
- * @return uint32_t number of mismatches.
+ * @return uint32_t number of mismatches detected.
  */
 static uint32_t rule_si_fn_mismatch(const record_metadata *metadata);
+
+/**
+ * @brief Detects temporal-order violations such as accessed < created.
+ * @param metadata Pointer to the record_metadata structure containing parsed attributes.
+ * @return uint32_t number of rollbacks detected.
+ */
+static uint32_t rule_timestamp_rollback(const record_metadata *metadata);
 
 /**
  * @brief Detects if multiple timestamps have a variable number of trailing zeroes.
@@ -40,10 +48,17 @@ uint32_t rules_evaluate(const record_metadata *metadata)
     }
 
     uint32_t rule_si_fn_mismatch_count = rule_si_fn_mismatch(metadata);
-    
+
     if (rule_si_fn_mismatch_count >= SI_FN_MISMATCH_THRESHOLD)
     {
         rule_flags |= RULE_FLAG(RULE_SN_FN_MISMATCH);
+    }
+
+    uint32_t rule_timestamp_rollback_count = rule_timestamp_rollback(metadata);
+
+    if (rule_timestamp_rollback_count >= TIMESTAMP_ROLLBACK_THRESHOLD)
+    {
+        rule_flags |= RULE_FLAG(RULE_TIMESTAMP_ROLLBACK);
     }
 
     if (rule_zeroed_timestamp(metadata))
@@ -89,6 +104,38 @@ static uint32_t rule_si_fn_mismatch(const record_metadata *metadata)
     }
 
     return mismatches;
+}
+
+static uint32_t rule_timestamp_rollback(const record_metadata *metadata)
+{
+    if (metadata == NULL || !metadata->has_standard_information)
+    {
+        return 0;
+    }
+
+    uint32_t rollbacks = 0;
+
+    if (metadata->si.modified_time < metadata->si.creation_time)
+    {
+        rollbacks++;
+    }
+
+    if (metadata->si.mft_modified_time < metadata->si.modified_time)
+    {
+        rollbacks++;
+    }
+
+    if (metadata->si.accessed_time < metadata->si.creation_time)
+    {
+        rollbacks++;
+    }
+
+    if (metadata->fn.modified_time < metadata->fn.creation_time)
+    {
+        rollbacks++;
+    }
+
+    return rollbacks;
 }
 
 static bool rule_zeroed_timestamp(const record_metadata *metadata)
