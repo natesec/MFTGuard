93d253dc73ca615e54e3249af9c95d19f1a600e1
Nate <natewardbusiness@gmail.com>
2026-08-28 12:30:51 -0400

    feat: zeroed timestamp rule function implemented

--- src/rules.c
+++ src/rules.c
@@ -2,3 +2,52 @@
 #include <stdint.h>
 
 #include "rules.h"
+#include "attributes.h"
+#include "timestamps.h"
+
+#define ZEROED_TIMESTAMP_DIGITS 9
+#define ZEROED_TIMESTAMP_MINIMUM_MATCHES 2
+
+/**
+ * @brief Detects if multiple timestamps have a variable number of trailing zeroes.
+ * @param metadata Pointer to the record_metadata struct containing parsed attributes.
+ * @return true if set number of timestamps have the set number of trailing zeroes, false otherwise.
+ */
+static bool rule_zeroed_timestamp(const record_metadata *metadata);
+
+static bool rule_zeroed_timestamp(const record_metadata *metadata)
+{
+    if (metadata == NULL)
+    {
+        return false;
+    }
+
+    if (!metadata->has_standard_information)
+    {
+        return false;
+    }
+
+    uint32_t matches = 0;
+
+    if (timestamp_low_digits_zeroed(metadata->si.creation_time, ZEROED_TIMESTAMP_DIGITS))
+    {
+        matches++;
+    }
+
+    if (timestamp_low_digits_zeroed(metadata->si.modified_time, ZEROED_TIMESTAMP_DIGITS))
+    {
+        matches++;
+    }
+
+    if (timestamp_low_digits_zeroed(metadata->si.mft_modified_time, ZEROED_TIMESTAMP_DIGITS))
+    {
+        matches++;
+    }
+
+    if (timestamp_low_digits_zeroed(metadata->si.accessed_time, ZEROED_TIMESTAMP_DIGITS))
+    {
+        matches++;
+    }
+
+    return matches >= ZEROED_TIMESTAMP_MINIMUM_MATCHES;
+}
