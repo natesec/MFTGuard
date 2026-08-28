38d85aa4ebd67ec2b34c090cf3ac990e12179a65
Nate <natewardbusiness@gmail.com>
2026-08-28 12:14:03 -0400

    feat: create rule flag system with enum as bit positions

--- include/rules.h
+++ include/rules.h
@@ -4,4 +4,26 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+/**
+ * Used to bit shift rule IDs into respective positions marked
+ * by the rule_id enum.
+ */
+#define RULE_FLAG(id) (1u << (id))
+
+/**
+ * @brief Represents an ID for each detection rule.
+ * @note Used in the form of a bit position. These flags
+ *       will then undergo a bitwise OR operation to collapse
+ *       all detections into a single variable.
+ */
+typedef enum
+{
+    RULE_SN_FN_MISMATCH,
+    RULE_TIMESTAMP_ROLLBACK,
+    RULE_ZEROED_TIMESTAMP,
+    RULE_IDENTICAL_TIMESTAMPS,
+
+    RULE_COUNT
+} rule_id;
+
 #endif
