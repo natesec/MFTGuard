f750f9413443b00ffc338c681a24fcbacd3d2b53
Nate <natewardbusiness@gmail.com>
2026-09-10 12:17:36 -0400

    feat: implement rule flags scoring without contextual stats

--- src/scoring.c
+++ src/scoring.c
@@ -1,1 +1,27 @@
 #include "scoring.h"
+
+/** TODO: implement statistics-based weighted, contextual score */
+uint32_t scoring_score_rule_flags(uint32_t rule_flags, const statistics *stats)
+{
+    uint32_t score = 0;
+
+    if (rule_flags & RULE_FLAG(RULE_SI_FN_MISMATCH))
+    {
+        score += 10;
+    }
+
+    if (rule_flags & RULE_FLAG(RULE_ZEROED_TIMESTAMP))
+    {
+        score += 20;
+    }
+
+    if (rule_flags & RULE_FLAG(RULE_IDENTICAL_TIMESTAMPS))
+    {
+        score += 15;
+    }
+
+    if (rule_flags & RULE_FLAG(RULE_TIMESTAMP_ROLLBACK))
+    {
+        score += 5;
+    }
+}
